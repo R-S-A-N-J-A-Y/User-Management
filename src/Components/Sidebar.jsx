902154f35a7530a85c45033d420baf06d2b1325e
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-04-18 10:15:42 +0530

    Moved Logout to Profile page

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -4,13 +4,13 @@ import { useAuth } from "../Context/AuthContext";
 import { CgUserlane } from "react-icons/cg";
 
 const Sidebar = () => {
-  const { state, Logout } = useAuth();
+  const { state } = useAuth();
   return (
-    <nav className="sidebar p-5 navbar navbar-expand-lg d-flex flex-column justify-content-between align-items-center">
+    <nav className="sidebar p-4 navbar navbar-expand-lg d-flex flex-column justify-content-between align-items-center h-100">
       <div>
         <CgUserlane size={60} />
       </div>
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex flex-column justify-content-around align-items-start gap-5 w-100">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex flex-column justify-content-around align-items-start w-100 h-100">
         <li>
           <NavLink className="nav-link" to="/">
             Home
@@ -46,9 +46,6 @@ const Sidebar = () => {
               <NavLink className="nav-link" to="/profile">
                 Profile
               </NavLink>
-            </li>
-            <li>
-              <button onClick={Logout}>Logout</button>
             </li>
           </>
         )}
--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useAuth } from "../Context/AuthContext";
 
 const Profile = () => {
-  const { state, fetchProfile, isLoading } = useAuth();
+  const { state, fetchProfile, isLoading, Logout } = useAuth();
 
   useEffect(() => {
     if (!state.profile) {
@@ -15,6 +15,10 @@ const Profile = () => {
   return (
     <div>
       <h1>{state.profile?.personal.name}</h1>
+
+      <button className="btn btn-danger ms-2" onClick={Logout}>
+        Logout
+      </button>
     </div>
   );
 };
