1ee5b5f7509b9086882e01a6fac37c4a2aaee8fd
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-04-18 21:15:13 +0530

    Added Content for About and Home page.

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="p-5 w-100">
+      <h1 className="text-center">About Us</h1>
+    </div>
+  );
 };
 
 export default About;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="p-5 w-100">
+      <h1 className="text-center">Welcome To User Management App.</h1>
+      <p className="text-center fs-4">
+        Your all-in-one solution for managing users effortlessly.
+      </p>
+    </div>
+  );
 };
 
 export default Home;
